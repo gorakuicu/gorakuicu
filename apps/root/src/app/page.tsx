7ce31672857b47511dbed8800bc71dea0d806f58
Maxx Borer <maxxborer@gmail.com>
2023-06-17 17:23:55 +0600

    feat: soon

--- apps/root/src/app/page.tsx
+++ apps/root/src/app/page.tsx
@@ -5,12 +5,13 @@ import Structure from '@/features/layout/Structure';
 export default function Home() {
   return (
     <Structure>
-      <div
+      {/* <div
         className="flex min-h-screen flex-col items-center justify-between"
         style={{
           height: 2000,
         }}
-      />
+      /> */}
+      <h2 className="text-center text-9xl font-black">SOON</h2>
     </Structure>
   );
 }
